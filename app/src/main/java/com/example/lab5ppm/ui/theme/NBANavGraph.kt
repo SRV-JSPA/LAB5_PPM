a0094f192df55e956ba2c15c6151b045df18656c
JSPA <per22318@uvg.edu.gt>
2023-10-03 22:43:42 -0600

    cambios

--- app/src/main/java/com/example/lab5ppm/ui/theme/NBANavGraph.kt
+++ app/src/main/java/com/example/lab5ppm/ui/theme/NBANavGraph.kt
@@ -4,10 +4,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import androidx.navigation.NavDestination
+import androidx.navigation.NavHost
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 
 @Composable
 fun NBANavGraph(modifier:Modifier = Modifier, navegacion: ()-> Unit, navController: NavHostController= rememberNavController(),startDestination: String = screen.main.ruta){
+        NavHost(navController = navController, startDestination = startDestination, builder = modifier){
 
+        }
 }
